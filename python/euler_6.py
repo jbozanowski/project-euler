3bc539f10571c60c929248d60a97411ff1cd06f7
Jakub Bożanowski <jbozanowski@gmail.com>
2013-05-09 20:26:25 +0200

    Problem 6 solution in Python.

---
+++ python/euler_6.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def euler_6():
+    def _square_of_sum(max_int):
+        return sum(range(1, max_int+1)) ** 2
+
+    def _sum_of_squares(max_int):
+        return sum(map(lambda x: x * x, range(1, max_int+1)))
+
+    m = 100
+    return _square_of_sum(m) - _sum_of_squares(m)
+
+
+if __name__ == '__main__':
+    print euler_6()
