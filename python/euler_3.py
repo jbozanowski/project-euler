cb9327480f3606f78c91c87d399d54a33ad397ec
Jakub Bożanowski <jbozanowski@gmail.com>
2013-11-01 14:22:46 +0100

    Using is_prime from common library.

--- python/euler_3.py
+++ python/euler_3.py
@@ -9,21 +9,15 @@
 
 from math import sqrt
 
+from common import is_prime
+
 
 def euler_3():
     target = 600851475143
     max_factor = 0
-
-    def _check_prime(x):
-        limit = int(round(sqrt(x)))
-        for i in xrange(2, limit+1):
-            if x % i == 0:
-                return False
-        return True
-    
     target_limit = int(round(sqrt(target)))
     for i in xrange(2, target_limit+1):
-        if target % i == 0 and _check_prime(i) and i > max_factor:
+        if target % i == 0 and is_prime(i) and i > max_factor:
             max_factor = i
     return max_factor
 
